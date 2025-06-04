609013f3209999b80a18414cf5bde6f129d36248
CODA29 <rasdagm@gmail.com>
2025-06-03 21:01:00 -0700

    feat: added the quote.py file

---
+++ week_2/quote.py
@@ -0,0 +1,13 @@
+"""
+Author: Dagmawi Megra
+Date: 06/03/2025
+File Name: quote.py
+Description: This module prints a famous quote along with its author.
+"""
+
+# Defining the quote and author variables
+quote = "If you tell the truth, you don't have to remember anything."
+author = "Mark Twain"
+
+#Printing the quote
+print(f'"{quote}" - {author}')
